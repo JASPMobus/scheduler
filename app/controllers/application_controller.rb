091dfadc96786e8d8e9918bcda39c2a40857966b
learn <mobusj@live.com>
2019-12-10 17:39:23 -0800

    Add basic layout. To be customized later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < Sinatra::Base
 	configure do
 		set :views, 'app/views'
+		set :public_folder, 'public'
+		
+		enable :sessions
+		set :session_secret, SecureRandom.hex(20)
 	end
 
 	get "/" do
