7442887c700ccfbafbacba9f807f05e42aeaf893
learn <mobusj@live.com>
2019-12-10 18:26:54 -0800

    Add files for many basic pages. Add Sign Up Form. Start error handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
 	configure do
 		set :views, 'app/views'
 		set :public_folder, 'public'
+		set :layout, 'app/views/layout'
 		
 		enable :sessions
 		set :session_secret, SecureRandom.hex(20)
@@ -9,5 +10,31 @@ class ApplicationController < Sinatra::Base
 
 	get "/" do
 		erb :index
+	end
+
+	get "/signup" do
+		#TO ADD: check if user logged in
+		erb :signup 
+	end
+
+	post "/login" do
+		user = User.create(params)
+
+		if user.class != String
+			session[:user_id] = user.id
+			@session = session
+
+			redirect "/account"
+		else
+			redirect "/error/#{user}"
+		end
+	end
+
+	get "/account" do
+		erb :account
+	end
+
+	get "/error/:error" do 
+		erb :error
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,36 @@ class User < ActiveRecord::Base
     UPPER = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ")
     NUMBER = "1 2 3 4 5 6 7 8 9 0".split(" ")
 
+    def self.create(params)
+        if acceptable_password?(params[:password1])
+            if params[password1] == params[:password2]
+                user = User.new
+
+                if User.all.length == 0
+                    user.type = "admin"
+                else
+                    user.type = "user"
+                end
+                user.id = User.all.length + 1
+                user.first_name = params[:first_name]
+                user.last_name = params[:last_name]
+                user.username = params[:username]
+                user.password = params[:password1]
+                user.notes = ""
+
+                if user.save
+                    return user
+                else 
+                    return "account-creation"
+                end
+            else 
+               return "password-confirmation" 
+            end
+        else
+            return "bad-password"
+        end 
+    end
+
     #checks if the string has a lower case letter, an upper case letter, a number, and is at least 8 characters long
     def self.acceptable_password?(str)
         one_of_include?(str, LOWER) && one_of_include?(str, UPPER) && one_of_include?(str, NUMBER) && str.length >= 8
@@ -24,5 +54,5 @@ class User < ActiveRecord::Base
         chars.shift
         
         one_of_include?(str, chars)
-    end 
+    end
 end
