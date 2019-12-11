e3a67a57179177154e2584f42698590ec01d1fd8
learn <mobusj@live.com>
2019-12-10 16:39:09 -0800

    Add some comments to user.rb to clarify purpose of globals and description of method self#acceptable_password?.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
 
+    #used for self#acceptable_password?
     LOWER = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
     UPPER = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ")
     NUMBER = "1 2 3 4 5 6 7 8 9 0".split(" ")
 
+    #checks if the string has a lower case letter, an upper case letter, a number, and is at least 8 characters long
     def self.acceptable_password?(str)
         one_of_include?(str, LOWER) && one_of_include?(str, UPPER) && one_of_include?(str, NUMBER) && str.length >= 8
     end
