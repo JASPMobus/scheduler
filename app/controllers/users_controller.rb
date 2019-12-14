d5e8da779145e6c8b87d2f379bd24eb5f1768a67
JASPMobus <mobusj@live.com>
2019-12-14 15:53:27 -0800

    Fix layout issue. Begin introducing full usertype distinctions and user account level editing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,61 @@
 class UsersController < ApplicationController
 	configure do
-		set :public_folder, 'public'
-		set :layout, 'app/views/users/layout'
+		set :layout, 'app/views/layout'
 	end
+
 	get "/users" do
-		#Only admins can view the users page
-		if logged_in? && current_user.kind=="admin"
+		#Only employees can view the users page
+		if logged_in? && current_user.kind!="user"
 			@users = User.all
 
 			erb :'users/index'
 		#If you aren't logged in as an admin, you can't view it
 		else
-			redirect "/error/not-logged-in-as-admin"
+			redirect "/error/lacking-privileges"
 		end
 	end
 
     get "/users/:username" do
+		#Only employees can view the users page
+		if logged_in? && current_user.kind!="user"
         #Finds the user
-        @user = User.find_by(username: params[:username])
+		@user = User.find_by(username: params[:username])
 
-		#Renders the page if it exists, otherwise gives the error page
         if @user
             erb :'users/user'
         else
             redirect "/error/invalid-user-selected"
         end 
+		#If you aren't logged in as an admin, you can't view it
+		else
+			redirect "/error/lacking-privileges"
+		end
 	end
 
 	get "/users/:username/edit" do
-        #Finds the user
-        @user = User.find_by(username: params[:username])
+		#Only employees can view the users page
+		if logged_in? && current_user.kind!="user"
+			#Finds the user
+			@user = User.find_by(username: params[:username])
+			@viewer = current_user
 
-		#Renders the page if it exists, otherwise gives the error page
-        if @user
-            erb :'users/edit'
-        else
-            redirect "/error/invalid-user-selected"
-        end 
+			if @user
+				erb :'users/edit'
+			else
+				redirect "/error/invalid-user-selected"
+			end 
+			#If you aren't logged in as an admin, you can't view it
+		else
+			redirect "/error/lacking-privileges"
+		end
+	end
+
+	patch "/users/:username" do
+		#Finds the user
+		@user = User.find_by(username: params[:username])
+
+		@user.update(params)
+
+		redirect "/users/:username"
 	end
 end
