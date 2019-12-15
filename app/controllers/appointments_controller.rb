e28742b2e6b148b4c93cc576c958c0f1a169b384
JASPMobus <mobusj@live.com>
2019-12-15 12:49:27 -0800

    Finish temporary appointment form.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,7 +2,23 @@ class AppointmentsController < ApplicationController
     get "/appointments/new" do
         #grabs all of the providers
         @providers = User.all.filter { |user| user.kind == "provider" }
-        
+
         erb :'appointments/new'
+    end
+
+    post "/appointments" do
+        puts params
+
+        redirect "/appointments"
+    end
+
+    get "/appointments" do
+        if logged_in? && current_user.kind!="user"
+            @appointments = Appointment.all
+            
+            erb :'appointments/index'  
+        else
+            redirect "/error/lacking-privileges"
+        end
     end
 end
