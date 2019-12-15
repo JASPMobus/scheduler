5fec0781408c882dc23765f4271aa754446a82b2
JASPMobus <mobusj@live.com>
2019-12-15 11:26:27 -0800

    Add table skeleton to schedule page. Add some table style stuff.

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,5 +1,10 @@
 class ScheduleController < ApplicationController
-    get "/schedule" do
+		get "/schedule" do
+		#grabs all of the providers
+		@providers = User.all.filter { |user| user.kind == "provider" }
+
+		puts @providers
+
 		#The schedule page
 		erb :'schedule/index'
 	end
